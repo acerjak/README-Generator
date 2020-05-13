6216f9b727eeb8f81a50e88a9b12ec07af120628
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-13 00:40:46 -0700

    ability to search by username for git info

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
+const { prompt } = require( 'inquirer')
+const { writeFile, appendFile} = require('fs')
+const { promisify } = require('util')
+//bring in axios
+const axios = require ('axios')
+//promisify 
+const writeFilePromise = promisify(writeFile)
+const appendFilePromise = promisify(appendFile)
+
+//define start of api requests
+// let count = 0
 //bring over code on APIs
-let api = require('./api.js')
+// let api = require('./api.js')
 //constant for username question
-const firstQuestion = [
+//prompt user for GitHub Username
+prompt([
 {
     type: 'input',
     name: 'username',
     message: 'What is your GitHub username?'
 }
-];
+])
+//get data from GitHub API
+    .then(data=> {
+        //creat object to collect responses
+        let responses = {}
+        //using for in loop over object, take key=name from question
+        for(const key in data) {
+            //plug into API request
+            axios.get(`https://api.github.com/search/repositories?q=user:${data[key]}`)
+            //extract the data without having to call it data.data
+            .then(({data}) => {
+                // count++
+                // responses[key] = data
+                console.log(data)
+            })
+    .catch(err => console.log(err))
+  }})
 
-function writeToFile(fileName, data) {
-}
+// getUser(username);
 
-function init() {
+// function writeToFile(fileName, data) {
+// }
 
-}
+// function init() {
 
-init();
-module.exports = firstQuestion
+// }
+
+// init();
+
