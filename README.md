332441628159c3e3c06bc54822741d382c032ce7
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-13 20:25:38 -0700

    update to read me, check formatting

--- README.md
+++ README.md
@@ -1,51 +1,38 @@
 
-    # CodeQuiz
-    ---------------------------------------
+##CodeQuiz
+---------------------------------------
+##Description
+---------------------------------------
+Blah
+##Table of Contents
+---------------------------------------
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+##Installation
+---------------------------------------
+Please follow this command to install npm dependencies:
+npm i
+##Usage
+---------------------------------------
+Blah
+##Credits
+---------------------------------------
+Blah
+##License
+---------------------------------------
+None
+##Tests
+---------------------------------------
+npm test
+##Questions
+---------------------------------------
+![profilepic](https://avatars1.githubusercontent.com/u/62491401?v=4 "acerjak")
 
-    ## Description
-    ---------------------------------------
-    Brief quiz of 5 questions that is sorted randomly. 
-
-    ## Table of Contents
-    ---------------------------------------
-    * [Installation](#installation)
-
-    * [Usage](#usage)
-
-    * [License](#license)
-
-    * [Contributing](#contributing)
-
-    * [Tests](#tests)
-
-    * [Questions](#questions)
-
-    ## Installation
-    ---------------------------------------
-    Please follow this command to install npm dependencies:
-    npm i
-
-    ## Usage
-    ---------------------------------------
-    Press Start to begin quiz and at the end, log your score on the score board, which is based on time remaining.
-
-    ## Credits
-    ---------------------------------------
-    Google-fu, Web Dev Simplified, and I
-
-    ## License
-    ---------------------------------------
-    None
-
-    ## Tests
-    ---------------------------------------
-    npm test
-
-    ## Questions
-    ---------------------------------------
-    ![profile pic](https://avatars1.githubusercontent.com/u/62491401?v=4 "acerjak")
-    
-    Please send any inquiries or concerns through ![acerjak]("https://api.github.com/users/acerjak") at amandacerjak@gmail.com.
-    ---------------------------------------
-    Created by Amanda 2020
-     
+Please send any inquiries or concerns through ![acerjak]("https://api.github.com/users/acerjak") at amandacerjak@gmail.com.
+---------------------------------------
+Created by Amanda 2020
+ 
--- index.js
+++ index.js
@@ -100,56 +100,43 @@ prompt([
 // push data from api into this markdown function (md for readme)
 function generateMarkdown({responses, data}) {
     return `
-    # ${responses.title}
-    ---------------------------------------
+##${responses.title}
+---------------------------------------
+##Description
+---------------------------------------
+${responses.description}
+##Table of Contents
+---------------------------------------
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+##Installation
+---------------------------------------
+Please follow this command to install npm dependencies:
+${responses.installation}
+##Usage
+---------------------------------------
+${responses.usage}
+##Credits
+---------------------------------------
+${responses.contributing}
+##License
+---------------------------------------
+${responses.license}
+##Tests
+---------------------------------------
+${responses.test}
+##Questions
+---------------------------------------
+![profilepic](https://avatars1.githubusercontent.com/u/62491401?v=4 "acerjak")
 
-    ## Description
-    ---------------------------------------
-    ${responses.description}
-
-    ## Table of Contents
-    ---------------------------------------
-    * [Installation](#installation)
-
-    * [Usage](#usage)
-
-    * [License](#license)
-
-    * [Contributing](#contributing)
-
-    * [Tests](#tests)
-
-    * [Questions](#questions)
-
-    ## Installation
-    ---------------------------------------
-    Please follow this command to install npm dependencies:
-    ${responses.installation}
-
-    ## Usage
-    ---------------------------------------
-    ${responses.usage}
-
-    ## Credits
-    ---------------------------------------
-    ${responses.contributing}
-
-    ## License
-    ---------------------------------------
-    ${responses.license}
-
-    ## Tests
-    ---------------------------------------
-    ${responses.test}
-
-    ## Questions
-    ---------------------------------------
-    ![profile pic](https://avatars1.githubusercontent.com/u/62491401?v=4 "acerjak")
-    
-    Please send any inquiries or concerns through ![${responses.username}]("https://api.github.com/users/${responses.username}") at ${responses.email}.
-    ---------------------------------------
-    Created by Amanda 2020
-     `;
+Please send any inquiries or concerns through ![${responses.username}]("https://api.github.com/users/${responses.username}") at ${responses.email}.
+---------------------------------------
+Created by Amanda 2020
+ `;
     }
 
 
